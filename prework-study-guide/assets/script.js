46e7d4159f16afdc1648066fae054dc0527cd0d4
Tbroskii <103056212+Tbroskii@users.noreply.github.com>
2023-05-31 11:51:23 -0500

    added JS to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,27 @@
+
+/*
+let topic = "CS";
+
+if (topic === "HTML"){
+console.log("Let's study Html!");
+}
+else if (topic ==="CSS"){
+    console.log("Let's study CSS!");
+}
+else if (topic ==="Git"){
+    console.log("Let's study Git!");
+}
+else if (topic ==="Javascript"){
+    console.log("Let's study Javascript!");
+}
+else{
+    console.log("Please try again!");
+}
+*/
+
+let topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for (let i = 0; i < topics.length;i++ )
+{
+    console.log(topics[i]);
+}
